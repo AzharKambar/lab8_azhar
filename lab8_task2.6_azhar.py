c0d12297b4489c8e2732990a2346645aa76eeb6a
AzharKambar <124907708+AzharKambar@users.noreply.github.com>
2023-11-22 06:30:34 +0300

    Add files via upload

---
+++ lab8_task2.6_azhar.py
@@ -0,0 +1,9 @@
+class Episode:
+    def _init_(self, id, name, air_date, episode, characters, url, created):
+        self.id = id
+        self.name = name
+        self.air_date = air_date
+        self.episode = episode
+        self.characters = characters
+        self.url = url
+        self.created = created
